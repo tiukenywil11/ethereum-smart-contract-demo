1d44b1825588ffd06d3558f33da887a6b343ee32
Kenywil Tiu <tiukenywil11@gmail.com>
2022-01-11 22:52:47 +0800

    Section 2 Part 40: Populated compile.js initial code, for compiling solidity projects

--- Section2/boilerplate-inbox/inbox/compile.js
+++ Section2/boilerplate-inbox/inbox/compile.js
@@ -1,1 +1,8 @@
-// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSynx(inboxPath, 'utf8');
+
+console.log(solc.compile(source, 1));
