c85cda4b4e1b56ec1617942a44dd09ab9e545bc2
Kenywil Tiu <tiukenywil11@gmail.com>
2022-01-17 18:20:20 +0800

    Section 2 Part 47: Added test class Car, and initial test script describe Car

--- Section2/boilerplate-inbox/inbox/test/Inbox.test.js
+++ Section2/boilerplate-inbox/inbox/test/Inbox.test.js
@@ -2,3 +2,22 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car', () => {
+    it('can park', () => {
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    });
+});
+
+// add "test": "mocha" manually to package.json
