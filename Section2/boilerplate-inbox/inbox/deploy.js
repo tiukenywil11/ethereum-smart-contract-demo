bfefbeaa1c98977e2f77142dd163b42efbfc7cb0
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-02 23:00:14 +0800

    Section 2 Part 60: Added initial codes for deploy.js, providing with key phrase to an empty wallet, and Infura API link

--- Section2/boilerplate-inbox/inbox/deploy.js
+++ Section2/boilerplate-inbox/inbox/deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'whip brass stay lift tunnel river fun double that drive tiny truly',
+    'https://rinkeby.infura.io/v3/b6e6453ddb70482fa929f285c9202893'
+);
+
+const web3 = new Web3(provider);
