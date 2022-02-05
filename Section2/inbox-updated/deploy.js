5fb823ca6541eb40f21316e51755eb8bad288127
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-05 19:12:22 +0800

    Section 2 Part 66: Fixed folder structure, and added a new set of codes with Solidity 0.8.11 syntax

---
+++ Section2/inbox-updated/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'whip brass stay lift tunnel river fun double that drive tiny truly',
+    'https://rinkeby.infura.io/v3/b6e6453ddb70482fa929f285c9202893'
+);
+
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
