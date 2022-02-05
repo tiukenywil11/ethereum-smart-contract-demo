6dd30238c8c70b71c85d5c1b2452c625b3c70164
Kenywil Tiu <tiukenywil11@gmail.com>
2022-02-05 17:30:33 +0800

    Section 2 Part 61: Added function that gets account that contract will deploy to and from

--- Section2/boilerplate-inbox/inbox/deploy.js
+++ Section2/boilerplate-inbox/inbox/deploy.js
@@ -8,3 +8,16 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0]);
+    
+    await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    
+};
+deploy();
