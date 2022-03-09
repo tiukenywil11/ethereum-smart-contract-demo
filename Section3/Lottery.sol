08c7d8e93f99f32d30d9bda1cbbb98705b32babf
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-09 10:12:15 +0800

    Section 3 Part 76: Initialized dynamic players array, and added enter function to add to players array

--- Section3/Lottery.sol
+++ Section3/Lottery.sol
@@ -2,8 +2,13 @@ pragma solidity ^0.4.26;
 
 contract Lottery {
     address public manager;
+	address[] public players;
 
     constructor () public {
         manager = msg.sender;
     }
+	
+	function enter() public payable {
+		players.push(msg.sender);
+	}
 }
