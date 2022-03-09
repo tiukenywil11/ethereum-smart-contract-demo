689833305a2940e2c9bf9f067fe16ebee10fecbd
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-09 09:38:09 +0800

    Section 3 Part 75: Added dynamic string array, proving it's not supported by Solidity yet

--- Section3/ArrayTest.sol
+++ Section3/ArrayTest.sol
@@ -9,18 +9,31 @@ contract ArrayTest {
     // Initialize a dynamic array with uint values
     // Does not return the whole array when deployed, needs to input index when getting called
     uint[] public myArray;
+	/* Dynamic string array is not supported by Solidity yet
+	string[] public myArrayString;
+	*/
 
     constructor () {
         myArray.push(1);
         myArray.push(10);
         myArray.push(30);
+		/* Dynamic string array is not supported by Solidity yet
+        myArrayString.push("hi");
+		*/
     }
 
-    // This function returns the whole array, compared to line 7
+    // This function returns the whole array, compared to line 11
     function getMyArray() public view  returns (uint[] memory) {
         return myArray;
     }
- 
+
+	/*
+    -- This function won't compile giving a TyprError
+    function getMyArrayString() public view  returns (string[] memory) {
+        return myArrayString;
+    }
+	*/
+	
     function getArrayLength() public view returns (uint) {
         return myArray.length;    
     }
