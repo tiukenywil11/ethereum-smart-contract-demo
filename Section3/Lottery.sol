0c8d62be20de0098394fe4f8bad9cfdee9a73245
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-09 16:11:03 +0800

    Section 3 Part 77: Added payable tag to constructor, and added require for validation to enter function

--- Section3/Lottery.sol
+++ Section3/Lottery.sol
@@ -4,11 +4,15 @@ contract Lottery {
     address public manager;
 	address[] public players;
 
-    constructor () public {
+    // msg is a global variable that includes data from transaction.
+    constructor () public payable{
         manager = msg.sender;
     }
 	
 	function enter() public payable {
+        // require is a function that checks for requirements, before executing a function
+        // require the sender to send at least 0.1 ether to enter.
+        require(msg.value > .01 ether);
 		players.push(msg.sender);
 	}
 }
