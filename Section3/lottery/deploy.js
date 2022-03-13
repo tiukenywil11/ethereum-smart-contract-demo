b14fa7c3f991215c4837da82c1631d6661b5ac82
Kenywil Tiu <tiukenywil11@gmail.com>
2022-03-13 15:23:50 +0800

    Section 3 Part 88: Appended the following javascript files with boilerplate from Section 2: compile.js, deploy.js, package.json, package-lock.json

--- Section3/lottery/deploy.js
+++ Section3/lottery/deploy.js
@@ -3,23 +3,22 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+    'whip brass stay lift tunnel river fun double that drive tiny truly',
+    'https://rinkeby.infura.io/v3/b6e6453ddb70482fa929f285c9202893'
 );
+
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0]);
+    
+    result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi There'] })
+        .send({ gas: '1000000', from: accounts[0] });
 
-  console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
-
-  console.log('Contract deployed to', result.options.address);
-  provider.engine.stop();
+    console.log('Contract deployed to', result.options.address);
+    provider.engine.stop();
 };
 deploy();
