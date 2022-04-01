69b2a020c5fb3c9283ab87bf665c985d76d475f7
Kenywil Tiu <tiukenywil11@gmail.com>
2022-04-01 17:13:16 +0800

    Section 4 Part 109: Set up on submit function for forms

--- Section4/lottery-react/src/components/pages/Form.jsx
+++ Section4/lottery-react/src/components/pages/Form.jsx
@@ -1,22 +1,36 @@
 import { useState } from 'react'
+import web3 from '../config/web3';
+import lottery from '../smart-contract/lottery';
 
 function Form() {
 
-    const [ text, setText ] = useState('')
+    const [ ether, setEther ] = useState('')
 
     const onChange = e => {
-        setText(e.target.value);
+        setEther(e.target.value);
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        const accounts = await web3.eth.getAccounts();
+
+        // call method enter to send to the lottery contract
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei( ether, 'ether')
+        })
     }
 
     return (
         <>
-        <form>
+        <form onSubmit={onSubmit}>
             <h4> Want to try your luck? </h4>
             <div>
                 <label> Amount of ether to enter </label>
                 <input
                     type='text'
-                    value={text}
+                    value={ether}
                     onChange={onChange}
                 />
             </div>
